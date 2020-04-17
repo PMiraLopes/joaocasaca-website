bba23cc2178706507dc6917f3eef3f31893c585d
Pedro Lopes <pfmiralopes@gmail.com>
2020-04-17 15:31:00 +0100

    add test links

--- src/routes/NotFound/index.tsx
+++ src/routes/NotFound/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import {useHistory} from 'react-router-dom'
 
 const NotFound: React.FC = () => {
+  const history = useHistory()
   return (
     <section>
       <div>
         <h1>Page not found</h1>
         <div>Image</div>
+        <div onClick={() => history.push('/resume')}>resume</div>
+        <div onClick={() => history.push('/')}>homepage</div>
+        <div onClick={() => history.push('/project/name')}>project</div>
       </div>
     </section>
   )
