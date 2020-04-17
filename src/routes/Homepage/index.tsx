3db50d3c262837f842bd881defd525c33251c919
Pedro Lopes <pfmiralopes@gmail.com>
2020-04-17 14:27:24 +0100

    Add base routes for the website

--- src/routes/Homepage/index.tsx
+++ src/routes/Homepage/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import styles from './styles.module.scss'
 
 const Homepage: React.FC = () => {
-  return <section>Homepage</section>
+  return (
+    <section className={styles['a-homepage']}>
+      <h1 className={styles['a-homepage__title']}>Homepage</h1>
+    </section>
+  )
 }
 
 export default Homepage
--- src/routes/NotFound/index.tsx
+++ src/routes/NotFound/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
 const NotFound: React.FC = () => {
-  return <section>NotFound</section>
+  return (
+    <section>
+      <div>
+        <h1>Page not found</h1>
+        <div>Image</div>
+      </div>
+    </section>
+  )
 }
 
 export default NotFound
